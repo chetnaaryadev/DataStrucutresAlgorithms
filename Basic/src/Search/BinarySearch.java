b3656260a8595f75125d53ba7cf90539c7dac9b3
chetnaaryadev <chetna.arya.dev@gmail.com>
2026-04-10 21:25:12 +0530

    last occurrence in an array

--- Basic/src/Search/BinarySearch.java
+++ Basic/src/Search/BinarySearch.java
@@ -11,6 +11,8 @@ public class BinarySearch {
         System.out.println("Index of number 15 in array is : "+ res);
         System.out.println("First occurrence of number "+ a+" in array is : "+ firstOccurence(arr1,a));
         System.out.println("First occurrence of number "+ b+" in array is : "+ firstOccurenceOptimized(arr3,b));
+        System.out.println("last occurrence of number "+ a+" in array is : "+ lastOccurrenceOptimized(arr1,a));
+
     }
 
     public static int bSearch(int[] arr, int num) {
@@ -49,10 +51,30 @@ public class BinarySearch {
             else if(arr[mid]< num)
                 low = mid+1;
             else {
-                if(mid == 0 || arr[mid] != arr[mid])
+                if(mid == 0 || arr[mid-1] != arr[mid])
                     return mid;
                 else
                     high = mid -1;
+            }
+        }
+        return -1;
+    }
+
+    public static int lastOccurrenceOptimized(int[] arr, int num) {
+        int n = arr.length;
+        int high = n-1, low =0;
+        int mid = 0;
+        while(low<=high){
+            mid = (high + low)/2;
+            if(arr[mid]> num)
+                high = mid -1;
+            else if(arr[mid]< num)
+                low = mid+1;
+            else {
+                if(mid != n-1 || arr[mid] != arr[mid+1])
+                    return mid;
+                else
+                    low = mid + 1;
             }
         }
         return -1;
