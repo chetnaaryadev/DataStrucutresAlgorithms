87f1dae666d12c4a1377a08228fb43c4297c3fb5
chetnaaryadev <chetna.arya.dev@gmail.com>
2026-04-29 10:06:18 +0530

    median in two sorted array

--- Basic/src/Search/BinarySearch.java
+++ Basic/src/Search/BinarySearch.java
@@ -224,6 +224,29 @@ public class BinarySearch {
         return false;
     }
 
+    public static int medianOfTwoSortedArray(int[] arr, int[] arr2) {
+        int n1 = arr.length, n2 = arr2.length;
+        int begin1 =0, end1=n1;
+        while (begin1 <= end1){
+            int i1 = (begin1+end1)/2;
+            int i2 = (n1+n2+1)/2 -i1;
+            int min1 = (i1 == n1) ? Integer.MAX_VALUE : arr[i1];
+            int max1 = (i1 == 0) ? Integer.MIN_VALUE : arr[i1-1];
+            int min2 = (i2 == n2) ? Integer.MAX_VALUE : arr2[i2];
+            int max2 = (i2 == 0) ? Integer.MIN_VALUE : arr2[i2-1];
+            if(max2<=min1 && max1 <= min2) {
+                if ((n1 + n2) % 2 == 0) {
+                    return (Math.max(max1, max2) + Math.min(min1, min2) / 2);
+                }else{
+                    return Math.max(max1, max2);
+                }
 
+            }else if(max1 > min2){
+                end1 = i1-1;
+} else{
+begin1 = i1+1;
+}
 
 }
+        return -1;
+    }}
