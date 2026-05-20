c59ef987b38fbf0b6627ead4fdb28bf5ca925625
chetnaaryadev <chetna.arya.dev@gmail.com>
2026-05-20 23:50:47 +0530

    repeating elements with floyd cycle detection

--- Basic/src/Search/Searching.java
+++ Basic/src/Search/Searching.java
@@ -9,7 +9,9 @@ public class Searching {
         int[] arr = {0, 2, 1, 3, 2,2};
         int[] arr1 = {10, 15, 15, 18, 20, 20, 20};
 
-        System.out.println("Repeating element is : " + repeatingElement1(arr,6));
+        System.out.println("Repeating element is : " + repeatingElementOptimized(arr,6));
+
+        System.out.println("Repeating element is : " + repeatingElementWithoutOnSpace(arr,6));
     }
     public static int repeatingElement(int[] arr, int l){
         Map<Integer,Integer> map = new HashMap<>();
@@ -34,5 +36,22 @@ public class Searching {
         }
 
         return -1;
+    }
+
+    public static int repeatingElementWithoutOnSpace(int[] arr, int n) {
+       int slow = arr[0]+1;
+       int fast = arr[0]+1;
+       do{
+           slow = arr[slow]+1;
+           fast = arr[arr[fast]+1]+1;
+       }while(slow != fast);
+         slow = arr[0]+1;
+
+        while(slow != fast){
+            slow = arr[slow]+1;
+            fast = arr[fast]+1;
+        }
+        return slow-1;
+
     }
 }
