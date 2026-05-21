7a3d3e2cd34609dd3580373361d343a6dbbcc8b6
chetnaaryadev <chetna.arya.dev@gmail.com>
2026-05-21 22:45:41 +0530

    finding minimum of maximum pages that can be allocated to a student

--- Basic/src/Search/Searching.java
+++ Basic/src/Search/Searching.java
@@ -54,4 +54,41 @@ public class Searching {
         return slow-1;
 
     }
+
+    public static int minPages(int[] arr, int k) {
+        int n = arr.length;
+        int max =0, sum=0;
+        for(int i: arr){
+            sum += i;
+            max = Math.max(max,i);
+        }
+
+        int low = 0, high = sum, res = 0;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(isFeasible(arr,n,k,mid)){
+                res = mid;
+                high = mid -1;
+            }else{
+                low = mid + 1;
+
+            }
+        }
+        return res;
+
+
+    }
+
+    private static boolean isFeasible(int[] arr, int n, int k, int mid) {
+        int req = 1, sum =0;
+        for(int i=0;i<n;i++){
+            if(sum+arr[i]>mid){
+                req++;
+                sum=arr[i];
+            }else{
+                sum +=arr[i];
+            }
+        }
+        return req<= k;
+    }
 }
